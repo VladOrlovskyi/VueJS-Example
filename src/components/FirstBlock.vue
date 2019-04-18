50fec9de96b34c1c007f8da2279d791637f66fda
VladOrlovskiy <dxvardar@gmail.com>
2019-04-18 23:17:14 +0300

    VueJS update

--- src/components/FirstBlock.vue
+++ src/components/FirstBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="button_menu_1">
-      <button><img src="../../images/play-icon.svg" width="16" height="16"></button>
+      <button @click="autoBlock()"><img src="../../images/play-icon.svg" width="16" height="16"></button>
       <button @click="seen = true"><img src="../../images/plus-icon.svg" width="16" height="16">New</button>
       <button @click="resetInput()"><img src="../../images/cancel-icon.svg" width="16" height="16">Delete All</button>
       <button @click="repostInput()"><img src="../../images/repost-right-icon.svg" width="16" height="16">Repost All
@@ -11,20 +11,23 @@
           <li id="li_txt" v-for="(block1, idx) in blocks1" :key="idx">{{block1.text}}<br>
             <input name="message_txt" id="inputTxt" type="text" size="3" class="input"></li>
         </ul>
+        <ul>
+          <li class="li_repost" v-for="(block3, indx) in blocks3" :key="`A-${indx}`">{{block3}}</li>
+        </ul>
       </div>
     </div>
     <div>
       <div class="flex_box">
         <div>Search result: Total:</div>
         <div id="button_menu_2">
-          <button><img src="../../images/repost-left-icon.svg" width="16" height="16">Undo</button>
-          <button><img src="../../images/clear-icon.svg" width="16" height="16">Clear</button>
+          <button @click="undoBlock()"><img src="../../images/repost-left-icon.svg" width="16" height="16">Undo</button>
+          <button @click="clearBlock()"><img src="../../images/clear-icon.svg" width="16" height="16">Clear</button>
         </div>
       </div>
-      <ul>
-        <li v-for="(block1, idx) in blocks1" :key="idx">{{block1.text}}<br>
-          <div id="txt_block"></div>
-        </li>
+      <ul id="ul_auto">
+        <li v-for="(block2, idxx) in blocks2" :key="`B-${idxx}`">{{block2.text}}</li>
+        <br>
+        <li class="li_auto" id="auto_li" v-for="(block4, indxx) in blocks4" :key="`C-${indxx}`">{{block4}}</li>
       </ul>
     </div>
   </div>
@@ -52,9 +55,31 @@
           {text: 'Alarm'},
           {text: 'Actions'}
         ],
+        blocks2: [
+          {text: 'Rate'},
+          {text: 'F'},
+          {text: 'Source'},
+          {text: 'Age'},
+          {text: 'Avail'},
+          {text: 'Truck'},
+          {text: 'Truck-O'},
+          {text: 'DH-O'},
+          {text: 'F/P'},
+          {text: 'Origin'},
+          {text: 'Trip'},
+          {text: 'Destination'},
+          {text: 'DH-D'},
+          {text: 'Truck-D'},
+          {text: 'Lenght'},
+          {text: 'Weight'},
+          {text: 'Company'},
+          {text: 'Phone'}
+        ],
+        blocks3: [],
+        blocks4: [],
         seen: false,
         resetInput () {
-          var inputValue = document.getElementById('ul_txt').getElementsByTagName('input')
+          let inputValue = document.getElementById('ul_txt').getElementsByTagName('input')
           for (let i = 0; i < inputValue.length; i++) {
             if (inputValue[i].type === 'text') {
               console.log(inputValue[i].value)
@@ -63,13 +88,28 @@
           }
         },
         repostInput () {
+          let inputValueRepost = document.getElementById('ul_txt').getElementsByTagName('input')
+          for (let i = 0; i < inputValueRepost.length; i++) {
+            if (inputValueRepost[i].type === 'text') {
+              console.log(inputValueRepost[i].value)
+              this.blocks3.push(inputValueRepost[i].value)
+              console.log(this.blocks3[i])
+            }
+          }
+        },
+        clearBlock () {
+          this.blocks4 = []
 
+        },
+        undoBlock () {
+          this.blocks4.pop()
+        },
+        autoBlock () {
+          alert("WORK! WTF???")
         }
       }
     }
   }
-
-
 </script>
 
 <style scoped>
